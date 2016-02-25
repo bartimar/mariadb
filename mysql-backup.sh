1dd0ad6785382957daaf63312b8abde9b106baa4
Marek Bart <marek.bartik@ackee.cz>
2016-02-25 14:40:57 +0000

    volume check /var/lib/mysql feature

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -9,5 +9,17 @@ fi
 sed -i "s/%%S3_ACCESS_KEY%%/$S3_ACCESS_KEY/g" /root/.s3cfg
 sed -i "s/%%S3_SECRET_KEY%%/$S3_SECRET_KEY/g" /root/.s3cfg
 
+# volume check
+vol_path=$(mount | grep nfs4 | cut -d\  -f3)
+vol_path=${vol_path%/} # delete trailing slash if any
+
+if [[ "$vol_path" != '/var/lib/mysql' ]] 
+then
+     echo "ERROR: Volume is not mounted on /var/lib/mysql/"
+     echo "Start your container again with /var/lib/mysql/ mounted"
+     exit 2
+fi
+
+
 # add cron job
 echo '0 2 * * * root mysqldump -u root -p$MYSQL_ROOT_PASSWORD --all-databases --single-transaction --force > /tmp/alldb.sql && s3cmd put /tmp/alldb.sql s3://ackee-backups/$S3_BUCKET_DIR/' >> /etc/crontab
