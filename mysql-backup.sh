32af03e344651f365ff014e00fe6641de5fa19a7
Marek Bart <marek.bartik@ackee.cz>
2016-02-25 16:05:57 +0000

    feature = more volumes, check if at least one is ok

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -10,16 +10,29 @@ sed -i "s/%%S3_ACCESS_KEY%%/$S3_ACCESS_KEY/g" /root/.s3cfg
 sed -i "s/%%S3_SECRET_KEY%%/$S3_SECRET_KEY/g" /root/.s3cfg
 
 # volume check
+old_IFS=$IFS
+IFS=$'\n' # to ensure space-in-names are treated correctly in for loop
+# actually Bluemix console (GUI) does not allow you to have whitespaces in volume names
+
 vol_path=$(mount | grep nfs4 | cut -d\  -f3)
-vol_path=${vol_path%/} # delete trailing slash if any
+found=0
 
-if [[ "$vol_path" != '/var/lib/mysql' ]] 
+for i in $vol_path
+do
+ 
+ i=${i%/} # delete trailing slash 
+ [[ "$i" == '/var/lib/mysql' ]] && found=1
+
+done
+
+IFS=${old_IFS}
+
+if [ $found == 0 ] 
 then
-     echo "ERROR: Volume is not mounted on /var/lib/mysql/"
-     echo "Start your container again with /var/lib/mysql/ mounted"
-     exit 2
-fi
-
+      echo "ERROR: Volume is not mounted on /var/lib/mysql/"
+      echo "Start your container again with /var/lib/mysql/ mounted"
+      exit 2
+ fi
 
 # add cron job
 echo '0 2 * * * root mysqldump -u root -p$MYSQL_ROOT_PASSWORD --all-databases --single-transaction --force > /tmp/alldb.sql && s3cmd put /tmp/alldb.sql s3://ackee-backups/$S3_BUCKET_DIR/' >> /etc/crontab
