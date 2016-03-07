50db14a6be1ebefb3d5b2eda22833cfe58801210
Josef Gattermayer <josef.gattermayer@ackee.cz>
2016-03-07 11:55:29 +0100

    every hour

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -9,5 +9,5 @@ fi
 sed -i "s/%%S3_ACCESS_KEY%%/$S3_ACCESS_KEY/g" /root/.s3cfg
 sed -i "s/%%S3_SECRET_KEY%%/$S3_SECRET_KEY/g" /root/.s3cfg
 
-# add cron job
+# add cron job every hour
 echo '0 * * * * root mysqldump -u root -p$MYSQL_ROOT_PASSWORD --all-databases --single-transaction --force > /tmp/alldb.sql && s3cmd put /tmp/alldb.sql s3://ackee-backups/$S3_BUCKET_DIR/' >> /etc/crontab
