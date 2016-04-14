d7a8fe874fe4a41d5d9e574f91fa46708e71c512
Marek Bartík <bartimar6@gmail.com>
2016-04-14 14:12:46 +0200

    host+port in mysqldump + env variables MYSQL_HOST and MYSQL_PORT

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 This image provides a cron daemon that runs daily backups from mysql (clustered or single instance) to Amazon S3.
 
 Following ENV variables must be specified:
- - `MYSQL_URL` contains the whole connection string for mysql command line client
+ - `MYSQL_HOST` contains the remote host (hostname or IP) connection string for mysqldump command line client option -h
+ - `MYSQL_PORT` contains the remote port number for mysqldump option -P
   - `mysqlserver.domain.com:27017` in case of a single instance
  - `MYSQL_ROOT_PASSWORD` password of user `root` who has access to all dbs
  - `S3_URL` contains address in S3 where to store backups
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 # verify variables
-if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o "$MYSQL_URL"]; then
-	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL, MYSQL_URL. No backups, no fun.'
+if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o "$MYSQL_HOST" -o "$MYSQL_PORT"]; then
+	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL, MYSQL_URL, MYSQL_PORT. No backups, no fun.'
 	exit 1
 fi
 
@@ -19,7 +19,7 @@ s3cmd ls "s3://$S3_URL" > /dev/null
    echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
 
 # add a cron job
-echo "$CRON_SCHEDULE root rm -rf /tmp/dump && mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --all-databases --single-transaction --force -h "$MYSQL_URL" | gzip > /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
+echo "$CRON_SCHEDULE root rm -rf /tmp/dump && mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" --all-databases --single-transaction --force -h "$MYSQL_HOST" -P "$MYSQL_PORT" | gzip > /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
 crontab /etc/crontab
 
 exec "$@"
